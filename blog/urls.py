5ccb89ac24689ea29b5e504b4dd42ad3a9b499f6
clepyladies <marissa@utterbergdatadev.com>
2018-05-09 05:30:00 -0400

    Tutorial extension homework: secure your website. Added user authentication, login, logout.

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.contrib import admin
 from . import views
 
 urlpatterns = [
@@ -9,4 +10,8 @@ urlpatterns = [
     url(r'^drafts/$', views.post_draft_list, name='post_draft_list'),
     url(r'^post/(?P<pk>\d+)/publish/$', views.post_publish, name='post_publish'),
     url(r'^post/(?P<pk>\d+)/remove/$', views.post_remove, name='post_remove'),
+    url(r'^admin/', admin.site.urls),
+    url(r'^accounts/login/$', views.login, name='login'),
+    url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
+    url(r'', include('blog.urls')),
 ]
