010f9675404c5308dcc5f72cc24d32e25faa77e6
clepyladies <marissa@utterbergdatadev.com>
2018-05-10 05:51:01 -0400

    Debug 2 of comment model - had def approved_comments in wrong class for views.py

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+#from django.shortcuts import render
 from django.utils import timezone
 from .models import Post, Comment
 from django.shortcuts import render, get_object_or_404
